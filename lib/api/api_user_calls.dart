ed9556ed53a62c423dfcd593cd9a0e70b14646ee
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-05 17:17:14 +0100

    adding register user method

--- lib/api/api_user_calls.dart
+++ lib/api/api_user_calls.dart
@@ -49,3 +49,35 @@ Future<int> loginUser(name, password) async {
 
   return response.statusCode;
 }
+
+Future<int> registerUser(name, password) async {
+  // final startOfUrl = Provider.of<ApiData>(context, listen: false).getApiUrl();
+
+  //TODO change to "real" address
+  String url =
+      Platform.isAndroid ? 'http://10.0.2.2:8081' : 'http://localhost:8080';
+  var urlParsed = Uri.parse('$url/login');
+
+  Map data = {
+    "username": name,
+    "password": password,
+  };
+
+  var body = json.encode(data);
+
+  debugPrint("awaiting response");
+  var response = await http.post(
+    urlParsed,
+    headers: {"Content-Type": "application/json"},
+    body: body,
+  );
+  debugPrint("received response: ${response.statusCode}");
+
+  // Map<String, dynamic> temp = json.decode(response.body);
+
+  //TODO receive key and update
+  // String? apiKey = temp['api_key'];
+  // Provider.of<ApiData>(context, listen: false).updateKey(apiKey);
+
+  return response.statusCode;
+}
