086623ba4641f2f72113bb6fe25dd044cf695843
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-08 17:02:20 +0100

    removing redundant comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:new_base/login_page.dart';
 import 'package:new_base/meplijst/mep_page_wrapper_widget.dart';
 import 'package:new_base/recepturen_widget.dart';
 import 'package:new_base/teams_page/teams_page_widget.dart';
+import 'package:provider/provider.dart';
 
 import 'Archive/archief_widget.dart';
+import 'api/api_general.dart';
 import 'homescreen/homescreen_widget.dart';
 import 'menukaarten_widget.dart';
 import 'meplijst/mep_lijst_widget.dart';
@@ -19,30 +21,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MEP-chat',
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/meplijst': (context) => const MepLijstWidget(),
-        '/archief': (context) => const ArchiefWidget(),
-        '/menukaarten': (context) => const MenuKaartenWidget(),
-        '/recepturen': (context) => const RecepturenWidget(),
-      },
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'MEP-chat Home Page'),
-    );
+    return ChangeNotifierProvider<ApiData>(
+        create: (_) => ApiData(),
+        builder: (context, child) {
+          return MaterialApp(
+            title: 'MEP-chat',
+            initialRoute: '/login',
+            routes: {
+              '/login': (context) => LoginScreen(),
+              '/meplijst': (context) => const MepLijstWidget(),
+              '/archief': (context) => const ArchiefWidget(),
+              '/menukaarten': (context) => const MenuKaartenWidget(),
+              '/recepturen': (context) => const RecepturenWidget(),
+            },
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const MyHomePage(title: 'MEP-chat Home Page'),
+          );
+        });
   }
 }
 
