1697ab1d4611e44df0319acdca343d2c4fb59b42
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-09 09:03:49 +0100

    adding logic to api url get()

--- lib/api/api_general.dart
+++ lib/api/api_general.dart
@@ -1,11 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class ApiData extends ChangeNotifier {
-  String? apiKey = "empty";
+  String? apiKey = "";
 
   String getApiUrl() {
     //TODO change to real url
-    return 'http://localhost:8080/register';
+    String url =
+        Platform.isAndroid ? 'http://10.0.2.2:8081' : 'http://localhost:8081';
+    return url;
   }
 
   String? getApiKey() {
