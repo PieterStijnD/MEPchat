3f042b5777c3e0a6b86b6058d8c7baa40ee252f5
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-11 15:45:17 +0100

    adding meplijst fetch as ListItem logic

--- lib/api/api_meplijsten.dart
+++ lib/api/api_meplijsten.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
+import '../meplijst/mep_lijst_widget.dart';
 import 'api_general.dart';
 
 Future<List<MepLijstData>> getMepLijstenFromServer(context) async {
@@ -28,6 +29,42 @@ Future<List<MepLijstData>> getMepLijstenFromServer(context) async {
     list.add(MepLijstData.fromJson(item));
   }
   return list;
+}
+
+Future<List<Item>> getMepLijstenFromServerAsListItems(context) async {
+  String startOfUrl = Provider.of<ApiData>(context, listen: false).getApiUrl();
+
+  var url = Uri.parse('http://10.0.2.2:8081/mep-lijst');
+
+  String key = Provider.of<ApiData>(context, listen: false).apiKey!;
+
+  var response = await http.get(
+    url,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": 'Bearer $key',
+    },
+  );
+
+  var decodedData = jsonDecode(response.body);
+  //TODO improve logic, make more neat
+  List<MepLijstData> list = [];
+  for (var item in decodedData) {
+    list.add(MepLijstData.fromJson(item));
+  }
+  List<Item> list2 = [];
+  for (var item in list) {
+    return List<Item>.generate(list.length, (int index) {
+      return Item(
+        isExpanded: false,
+        isActive: true,
+        headerValue: item.name!,
+        expandedValue: 'This is item number $index',
+      );
+    });
+  }
+
+  return list2;
 }
 
 void deleteMepLijst(context) async {
