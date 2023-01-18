612db3d23942d345f347ba0ef0831338fdbae49a
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-18 10:53:51 +0100

    removing comments and unnecessary code

--- lib/meplijst/mep_lijst_widget.dart
+++ lib/meplijst/mep_lijst_widget.dart
@@ -155,16 +155,11 @@ class _MepLijstWidgetState extends State<MepLijstWidget> {
 
   Widget _buildSlidable(Item data, int i) {
     return Slidable(
-      // Specify a key if the Slidable is dismissible.
       key: ValueKey(i),
       startActionPane: ActionPane(
-        // A motion is a widget used to control how the pane animates.
         motion: const ScrollMotion(),
-        // A pane can dismiss the Slidable.
         dismissible: DismissiblePane(onDismissed: () {}),
-        // All actions are defined in the children parameter.
         children: const [
-          // A SlidableAction can have an icon and/or a label.
           SlidableAction(
             onPressed: null,
             backgroundColor: Color(0xFFFE4A49),
@@ -178,8 +173,6 @@ class _MepLijstWidgetState extends State<MepLijstWidget> {
         motion: ScrollMotion(),
         children: [
           SlidableAction(
-            // An action can be bigger than the others.
-            flex: 2,
             onPressed: null,
             backgroundColor: Color(0xFF7BC043),
             foregroundColor: Colors.white,
@@ -188,8 +181,6 @@ class _MepLijstWidgetState extends State<MepLijstWidget> {
           ),
         ],
       ),
-      // The child of the Slidable is what the user sees when the
-      // component is not dragged.
       child: ListTile(
         title: Text('${data.headerValue}'),
         // onTap: () {
@@ -301,157 +292,6 @@ class _MepLijstWidgetState extends State<MepLijstWidget> {
       ),
     );
   }
-
-  // void showMepDialog(BuildContext context) {
-  //   final MEPController = TextEditingController();
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return SimpleDialog(
-  //         title: Center(
-  //           child: Text("Toevoegen MEP"),
-  //         ),
-  //         children: [
-  //           Form(
-  //             key: _formKey,
-  //             child: Column(
-  //               // crossAxisAlignment: CrossAxisAlignment.stretch,
-  //               children: [
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(8.0),
-  //                   child: Text("Naam"),
-  //                 ),
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(8.0),
-  //                   child: Container(
-  //                     decoration: BoxDecoration(
-  //                       color: Colors.white,
-  //                       borderRadius: BorderRadius.only(
-  //                           topLeft: Radius.circular(10),
-  //                           topRight: Radius.circular(10),
-  //                           bottomLeft: Radius.circular(10),
-  //                           bottomRight: Radius.circular(10)),
-  //                       boxShadow: [
-  //                         BoxShadow(
-  //                           color: Colors.grey.withOpacity(0.5),
-  //                           spreadRadius: 5,
-  //                           blurRadius: 7,
-  //                           offset: Offset(0, 3), // changes position of shadow
-  //                         ),
-  //                       ],
-  //                     ),
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-  //                       child: TextFormField(
-  //                         controller: MEPController,
-  //                         decoration: InputDecoration(
-  //                             border: InputBorder.none, hintText: "..."),
-  //                         // The validator receives the text that the user has entered.
-  //                         validator: (value) {
-  //                           if (value == null || value.isEmpty) {
-  //                             return 'Please enter some text';
-  //                           }
-  //                           return null;
-  //                         },
-  //                       ),
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(18.0),
-  //                   child: Text(
-  //                     "Of",
-  //                     style: TextStyle(fontSize: 18),
-  //                   ),
-  //                 ),
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(8.0),
-  //                   child: Text("Receptuur toevoegen"),
-  //                 ),
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(8.0),
-  //                   child: Container(
-  //                     decoration: BoxDecoration(
-  //                       color: Colors.white,
-  //                       borderRadius: BorderRadius.only(
-  //                           topLeft: Radius.circular(10),
-  //                           topRight: Radius.circular(10),
-  //                           bottomLeft: Radius.circular(10),
-  //                           bottomRight: Radius.circular(10)),
-  //                       boxShadow: [
-  //                         BoxShadow(
-  //                           color: Colors.grey.withOpacity(0.5),
-  //                           spreadRadius: 5,
-  //                           blurRadius: 7,
-  //                           offset: Offset(0, 3), // changes position of shadow
-  //                         ),
-  //                       ],
-  //                     ),
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-  //                       child: TextField(
-  //                         decoration: InputDecoration(
-  //                           border: InputBorder.none,
-  //                           hintText: 'zoek receptuur',
-  //                         ),
-  //                       ),
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.center,
-  //                   children: [
-  //                     Padding(
-  //                       padding: const EdgeInsets.all(8.0),
-  //                       child: ElevatedButton(
-  //                         style: ButtonStyle(
-  //                           backgroundColor:
-  //                               MaterialStateProperty.all(Colors.white),
-  //                         ),
-  //                         onPressed: () {
-  //                           Navigator.pop(context);
-  //                         },
-  //                         child: Icon(
-  //                           Icons.cancel_outlined,
-  //                           color: Colors.amberAccent,
-  //                         ),
-  //                       ),
-  //                     ),
-  //                     Padding(
-  //                       padding: const EdgeInsets.all(8.0),
-  //                       child: ElevatedButton(
-  //                         style: ButtonStyle(
-  //                             backgroundColor:
-  //                                 MaterialStateProperty.all(Colors.white)),
-  //                         onPressed: () {
-  //                           // Validate returns true if the form is valid, or false otherwise.
-  //                           if (_formKey.currentState!.validate()) {
-  //                             // If the form is valid, display a snackbar. In the real world,
-  //                             // you'd often call a server or save the information in a database.
-  //                             //TODO make AddItem into api request
-  //                             // addItem(MEPController.text, _data);
-  //                             ScaffoldMessenger.of(context).showSnackBar(
-  //                               const SnackBar(
-  //                                 content: Text('Processing Data'),
-  //                               ),
-  //                             );
-  //                             Navigator.pop(context);
-  //                           }
-  //                         },
-  //                         child: Icon(Icons.check_circle_outline,
-  //                             color: Colors.green),
-  //                       ),
-  //                     ),
-  //                   ],
-  //                 )
-  //               ],
-  //             ),
-  //           )
-  //         ],
-  //       );
-  //     },
-  //   );
-  // }
 
   void showFormDialog(BuildContext context) {
     final MEPController = TextEditingController();
