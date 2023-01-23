bcf5612c2d63c9b9e6e2bfc5d724f47735a4fe5b
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-23 16:07:54 +0100

    adding parser for given text

---
+++ lib/photo_page/photo_parser_widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:new_base/api/api_recipes.dart';
+
+class PhotoParserWidget extends StatefulWidget {
+  final List<String> sentences;
+
+  const PhotoParserWidget({required this.sentences});
+
+  @override
+  PhotoParserWidgetState createState() => PhotoParserWidgetState();
+}
+
+class PhotoParserWidgetState extends State<PhotoParserWidget> {
+  List<String> options = [
+    "name",
+    "volume",
+    "unit",
+    "description",
+    "instructions",
+    "ingredients",
+    "preparationTime",
+    "timeUnit",
+    "archived"
+  ];
+  late String selectedOption = options[0];
+
+  void _selectOption(String option) {
+    setState(() {
+      selectedOption = option;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: widget.sentences.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.sentences[index]),
+                  DropdownButton<String>(
+                    value: selectedOption,
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedOption = value!;
+                      });
+                    },
+                    items: options
+                        .map((String option) => DropdownMenuItem<String>(
+                              value: option,
+                              child: Text(option),
+                            ))
+                        .toList(),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+        ButtonBar(
+          alignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                int code = 0;
+                code = await postRecipe("title", context);
+                debugPrint(code.toString());
+                // if (code != 0) {
+                //   setState(() {
+                //     fetchedMepLijsten = getMepLijstenFromServerAsListItems(context);
+                //   });
+                // }
+                Navigator.pop(context);
+              },
+              child: const Text("Save"),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
