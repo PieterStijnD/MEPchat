98690704934fd3877de2a036ceec66b3c2d87679
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-26 08:49:02 +0100

    adding general archive widget

--- lib/archive/archief_widget.dart
+++ lib/archive/archief_widget.dart
@@ -1,4 +1,9 @@
+import 'package:delayed_display/delayed_display.dart';
 import 'package:flutter/material.dart';
+
+import 'menu_archive_widget.dart';
+import 'mep_list_archive_widget.dart';
+import 'recipe_archive_widget.dart';
 
 class ArchiefWidget extends StatefulWidget {
   const ArchiefWidget({Key? key}) : super(key: key);
@@ -8,20 +13,61 @@ class ArchiefWidget extends StatefulWidget {
 }
 
 class _ArchiefWidgetState extends State<ArchiefWidget> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    MepListArchiveWidget(),
+    MenuArchiveWidget(),
+    RecipeArchiveWidget(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Icon(
-            Icons.archive_outlined,
-            size: 200,
-          ),
-          CircularProgressIndicator(
-            color: Colors.black,
-          ),
-        ],
+    return Scaffold(
+      body: Container(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            DelayedDisplay(
+              delay: Duration(milliseconds: 200),
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  _onItemTapped(0);
+                },
+                label: const Text('MEP-lijsten'),
+              ),
+            ),
+            DelayedDisplay(
+              delay: Duration(milliseconds: 300),
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  _onItemTapped(1);
+                },
+                label: const Text('Menukaarten'),
+              ),
+            ),
+            DelayedDisplay(
+              delay: Duration(milliseconds: 400),
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  _onItemTapped(2);
+                },
+                label: const Text('Recepten'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
