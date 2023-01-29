fbce87328a162bd6d45ff89666f07cdf47edc102
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2023-01-29 15:07:10 +0100

    removing add button from archives

--- lib/archive/menu_archive_widget.dart
+++ lib/archive/menu_archive_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:go_router/go_router.dart';
 
 import '../api/api_menus.dart';
 
@@ -54,15 +53,15 @@ class _MenuArchiveWidgetState extends State<MenuArchiveWidget> {
                       return Text("Empty");
                     },
                   ),
-                  Center(
-                    child: IconButton(
-                      color: Colors.black,
-                      onPressed: () {
-                        showFormDialog(context);
-                      },
-                      icon: Icon(Icons.add),
-                    ),
-                  )
+                  // Center(
+                  //   child: IconButton(
+                  //     color: Colors.black,
+                  //     onPressed: () {
+                  //       showFormDialog(context);
+                  //     },
+                  //     icon: Icon(Icons.add),
+                  //   ),
+                  // )
                 ],
               ),
             ),
@@ -72,16 +71,16 @@ class _MenuArchiveWidgetState extends State<MenuArchiveWidget> {
     );
   }
 
-  void addItem(String title, context) async {
-    int code = 0;
-    code = await postMenu(title, context);
-    debugPrint(code.toString());
-    if (code != 0) {
-      setState(() {
-        fetchedMenuList = getMenusFromServer(context);
-      });
-    }
-  }
+  // void addItem(String title, context) async {
+  //   int code = 0;
+  //   code = await postMenu(title, context);
+  //   debugPrint(code.toString());
+  //   if (code != 0) {
+  //     setState(() {
+  //       fetchedMenuList = getMenusFromServer(context);
+  //     });
+  //   }
+  // }
 
   void removeItem(int id, BuildContext context) async {
     int code = 0;
@@ -169,137 +168,137 @@ class _MenuArchiveWidgetState extends State<MenuArchiveWidget> {
     );
   }
 
-  void showFormDialog(BuildContext context) {
-    final MEPController = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return SimpleDialog(
-          title: Center(
-            child: Text("Toevoegen MEP"),
-          ),
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Naam"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                        child: TextFormField(
-                          controller: MEPController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none, hintText: "..."),
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Text(
-                      "Of",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Receptuur toevoegen"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'zoek receptuur',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.white),
-                          ),
-                          onPressed: () {
-                            context.pop();
-                          },
-                          child: Icon(
-                            Icons.cancel_outlined,
-                            color: Colors.amberAccent,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.white)),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              addItem(MEPController.text, context);
-                              context.pop();
-                            }
-                          },
-                          child: Icon(Icons.check_circle_outline,
-                              color: Colors.green),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
-        );
-      },
-    );
-  }
+// void showFormDialog(BuildContext context) {
+//   final MEPController = TextEditingController();
+//   showDialog(
+//     context: context,
+//     builder: (BuildContext context) {
+//       return SimpleDialog(
+//         title: Center(
+//           child: Text("Toevoegen MEP"),
+//         ),
+//         children: [
+//           Form(
+//             key: _formKey,
+//             child: Column(
+//               // crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: [
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Text("Naam"),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.white,
+//                       borderRadius: BorderRadius.all(Radius.circular(10)),
+//                       boxShadow: [
+//                         BoxShadow(
+//                           color: Colors.grey.withOpacity(0.5),
+//                           spreadRadius: 5,
+//                           blurRadius: 7,
+//                           offset: Offset(0, 3), // changes position of shadow
+//                         ),
+//                       ],
+//                     ),
+//                     child: Padding(
+//                       padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+//                       child: TextFormField(
+//                         controller: MEPController,
+//                         decoration: InputDecoration(
+//                             border: InputBorder.none, hintText: "..."),
+//                         // The validator receives the text that the user has entered.
+//                         validator: (value) {
+//                           if (value == null || value.isEmpty) {
+//                             return 'Please enter some text';
+//                           }
+//                           return null;
+//                         },
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(18.0),
+//                   child: Text(
+//                     "Of",
+//                     style: TextStyle(fontSize: 18),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Text("Receptuur toevoegen"),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.white,
+//                       borderRadius: BorderRadius.all(Radius.circular(10)),
+//                       boxShadow: [
+//                         BoxShadow(
+//                           color: Colors.grey.withOpacity(0.5),
+//                           spreadRadius: 5,
+//                           blurRadius: 7,
+//                           offset: Offset(0, 3), // changes position of shadow
+//                         ),
+//                       ],
+//                     ),
+//                     child: Padding(
+//                       padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+//                       child: TextField(
+//                         decoration: InputDecoration(
+//                           border: InputBorder.none,
+//                           hintText: 'zoek receptuur',
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: ElevatedButton(
+//                         style: ButtonStyle(
+//                           backgroundColor:
+//                               MaterialStateProperty.all(Colors.white),
+//                         ),
+//                         onPressed: () {
+//                           context.pop();
+//                         },
+//                         child: Icon(
+//                           Icons.cancel_outlined,
+//                           color: Colors.amberAccent,
+//                         ),
+//                       ),
+//                     ),
+//                     Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: ElevatedButton(
+//                         style: ButtonStyle(
+//                             backgroundColor:
+//                                 MaterialStateProperty.all(Colors.white)),
+//                         onPressed: () {
+//                           if (_formKey.currentState!.validate()) {
+//                             addItem(MEPController.text, context);
+//                             context.pop();
+//                           }
+//                         },
+//                         child: Icon(Icons.check_circle_outline,
+//                             color: Colors.green),
+//                       ),
+//                     ),
+//                   ],
+//                 )
+//               ],
+//             ),
+//           )
+//         ],
+//       );
+//     },
+//   );
+// }
 }
